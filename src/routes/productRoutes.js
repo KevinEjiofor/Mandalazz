805c0c7a7004d7774e2747304f85126044f673db
KevinEjiofor <ejioforkelvin@gmail.com>
2024-12-05 08:47:31 +0100

    new update

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productController = require('../product/controllers/productController');
+const productController = require('../product/controllers/ProductController');
 
 
 const isAdmin = require('../middlewares/isAdmin');
