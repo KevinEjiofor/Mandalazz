18357868529a4e1c9b21b5ca00a09aba43b94d1f
KevinEjiofor <ejioforkelvin@gmail.com>
2025-02-18 13:19:21 +0100

    refactoring socket

--- src/adminSocket.js
+++ src/adminSocket.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client');
 
 
-const socket = io('http://localhost:5000');
+const socket = io('http://localhost:8383');
 
 // Join the adminRoom
 socket.emit('joinRoom', 'adminRoom');
--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.SOCKET_PORT;
 server.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
